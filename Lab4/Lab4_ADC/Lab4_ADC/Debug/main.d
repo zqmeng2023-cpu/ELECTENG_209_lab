6f848fed988fec53a490e85b515d95419062e370
zqmeng2023-cpu <zqmeng2023@outlook.com>
2026-09-16 14:42:08 +1200

    Complete Lab4 part4.4

--- Lab4/Lab4_ADC/Lab4_ADC/Debug/main.d
+++ Lab4/Lab4_ADC/Lab4_ADC/Debug/main.d
@@ -13,7 +13,7 @@ main.d main.o: .././main.c .././common.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././adc.h
+ .././adc.h .././usart.h
 
 .././common.h:
 
@@ -46,3 +46,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 .././adc.h:
+
+.././usart.h:
--- Lab4/Lab4_ADC/Lab4_ADC/main.c
+++ Lab4/Lab4_ADC/Lab4_ADC/main.c
@@ -11,30 +11,41 @@
 #include <util/delay.h>
 
 #include "adc.h"
+#include "usart.h"
 
-volatile uint16_t adc_raw;
-volatile uint16_t adc_mv;
+#define NUM_SAMPLES 40
 
+uint16_t adc0_values[NUM_SAMPLES];
+uint16_t adc1_values[NUM_SAMPLES];
 
 int main(void)
 {
     /* Replace with your application code */
 	adc_init();
 	usart_init(12);
+	uint8_t i;
+	
+	//Collect 40 examples form ADC0 and ADC1
+	for (i = 0; i < NUM_SAMPLES; i++)
+	{
+		adc0_values[i] = adc_convert_mv(adc_read(0));
+		adc1_values[i] = adc_convert_mv(adc_read(1));
+	}
+	
+	//Send all samples through UART after sampling
+	for(i = 0; i < NUM_SAMPLES; i++)
+	{
+		usart_transmit_uint16(adc0_values[i]);
+		usart_transmit(',');
+		usart_transmit_uint16(adc1_values[i]);
+		usart_transmit('\r');
+		usart_transmit('\n');
+		
+	}
+	
+	
     while (1) 
     {
-		adc_raw = adc_read(2);
-		adc_mv = adc_convert_mv(adc_raw);
-		
-		// Convert number to ASCII and transmit
-		usart_transmit_uint16(adc_mv);
-		
-		// Send unit
-		usart_transmit_string("mV\r\n");
-		
-		// Slow down output so it is easier to read
-		_delay_ms(100);
-		
     }
 	return 0;
 }
